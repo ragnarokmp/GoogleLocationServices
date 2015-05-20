c5b9c2ee96ed850f627424ac098643d10a21ec71
Andrea Cerra <andrea.cerra@me.com>
2015-05-20 12:54:58 +0200

    - Add FLP Last Location Activity and retrive LastLocation from FLP - Add correct extension (FLP_) in strings.xml

--- app/src/main/java/it/mobileprogramming/ragnarok/googleplayservices/FLPActivityMain.java
+++ app/src/main/java/it/mobileprogramming/ragnarok/googleplayservices/FLPActivityMain.java
@@ -1,9 +1,10 @@
 package it.mobileprogramming.ragnarok.googleplayservices;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class FLPActivityMain extends ActionBarActivity {
@@ -12,5 +13,23 @@ public class FLPActivityMain extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flp);
+
+        flpLastLocationActivity();
+    }
+
+    /**
+     * Start Activity for show  last location maintained by the system
+     * */
+    public void flpLastLocationActivity() {
+
+        final Button button  = (Button) findViewById(R.id.buttonLastLocation);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(FLPActivityMain.this, FLPActivityLastLocation.class);
+                startActivity(intent);
+            }
+        });
     }
 }
