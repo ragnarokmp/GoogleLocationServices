9ab264c94c41c618e417e7a1464b8a40f036df3c
Andrea Cerra <andrea.cerra@me.com>
2015-05-21 15:05:02 +0200

    Remove menu in Activity Recognition Activity also remove all layouts menu file

--- app/src/main/java/it/mobileprogramming/ragnarok/googleplayservices/ActivityRecognitionMain.java
+++ app/src/main/java/it/mobileprogramming/ragnarok/googleplayservices/ActivityRecognitionMain.java
@@ -63,28 +63,6 @@ public class ActivityRecognitionMain extends ActionBarActivity implements Google
         googleApiClient.disconnect();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_activity_recognition_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     //when Play Services connects attach to Activity Recognition service with ActivityRecognitionIntentService that receives the intents
     @Override
     public void onConnected(Bundle bundle) {
